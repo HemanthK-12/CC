ebba75956ec2381fb30de051855b5db85ea2f688
Sw!tchblade <134306795+HemanthK-12@users.noreply.github.com>
2024-06-25 22:10:46 +0530

    Time: 7 ms (83.17%), Space: 16.5 MB (66.59%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,3 +1,13 @@
+/*
+-- In a matrix,we iterate through all elements in order.
+-- When we encounter a 0 anywhere in a row, we set the FIRST element of the row to 0.
+-- When we encounter a 0 anywhere in a column, we set the FIRST element of the column to 0.
+-- Only for 0,0 this causes a problem, so to solve this, we take another variable zero_row(set it initially to false).
+-- When there is a 0 in the FIRST row, we set zero_row=true and if there is 0 in first column,we do it normally and set the first element if the column to 0(which is 0,0).
+-- After this, iterate through first row and wherever it is 0, make a loop and set full column to 0.Vice versa for first column.
+-- This is O(1) solution.
+*/
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix)
