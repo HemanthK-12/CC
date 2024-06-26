ac5311aa08271fc75c7664358a75e390480855dc
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-06-26 13:26:54 +0530

    Time: 0 ms (100%), Space: 14.5 MB (54.31%) - LeetHub

--- 0031-next-permutation/0031-next-permutation--optimal.cpp
+++ 0031-next-permutation/0031-next-permutation--optimal.cpp
@@ -1,35 +1,42 @@
+/*
+-- First of all, Understand that all permutations in a set(eg.[2,3,6]), when made into numbers(eg. 236) will be arraged in ascending order.
+-- So, the next permutation of a set will be the next smallest number in the order of all permutations.
+-- First, see the break point from where, every element is decreasing.This is the common prefix.
+-- Then, find the min number after the break point, which is larger than the break point.For this, keep on swapping the break point with every number after break point if it is greater than the pt.
+-- After this, just reverse all the numbers after the break point and attach it to the numbers before the break pt.This is the next permutation.
+*/
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n=nums.size();
-        int pt=-1;
+        int break_pt=-1;
 
         for(int i=n-2;i>=0;i--)
         {
             if(nums[i]<nums[i+1])
             {
-                pt=i;
+                break_pt=i;
                 break;
             }
         }
 
-        if(pt==-1)
+        if(break_pt==-1)
         {
             reverse(nums.begin(),nums.end());
             return;
         }
 
-        for(int i=n-1;i>pt;i--)
+        for(int i=n-1;i>break_pt;i--)
         {
-            if(nums[i]>nums[pt])
+            if(nums[i]>nums[break_pt])
             {
-                nums[i]=nums[pt]+nums[i];
-                nums[pt]=nums[i]-nums[pt];
-                nums[i]=nums[i]-nums[pt];
+                nums[i]=nums[break_pt]+nums[i];
+                nums[break_pt]=nums[i]-nums[break_pt];
+                nums[i]=nums[i]-nums[break_pt];
                 break;
             }
         }
         
-        reverse(nums.begin()+pt+1,nums.end());
+        reverse(nums.begin()+break_pt+1,nums.end());
     }
 };
