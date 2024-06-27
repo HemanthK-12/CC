b8ffe5974d37b4a113e6ca9ecc21fa1244764a5a
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-06-27 22:33:28 +0530

    Time: 0 ms (100%), Space: 9.9 MB (36.07%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors--better-O(2n).cpp
@@ -0,0 +1,29 @@
+/*
+-- We iterate through the nums array and count the number of zeroes & ones and store it in "zero" and "one" respectively.
+-- Then, we set the first "zero" elements to 0, next "one" elements to 1 and the remaining to 2.
+-- O(2n).
+*/
+class Solution {
+public:
+    void sortColors(vector<int>& nums)
+    {
+        int zero=0,one=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+                zero++;
+            else if(nums[i]==1)
+                one++;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i<zero)
+                nums[i]=0;
+            else if(i<zero+one)
+                nums[i]=1;
+            else
+                nums[i]=2;
+        }
+
+    }
+};
