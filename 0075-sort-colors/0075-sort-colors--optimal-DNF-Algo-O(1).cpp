3a61266ea35d6d37878fce32bd23fd3424e8d5e3
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-06-28 01:27:40 +0530

    Time: 0 ms (100%), Space: 9.9 MB (36.07%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors--optimal-DNF-Algo-O(1).cpp
@@ -0,0 +1,47 @@
+/*
+-- We do this via Dutch National Flag Algorithm.
+-- We initialize 3 pointers -- low,mid,high.
+-- If 0<num<low-1, num should be 0.(I)
+-- If low<num<mid-1, num should be 1.(II)
+-- If mid<num<high, num should be in any random unsorted order.(III)
+-- If high+1<num<n-1, num should be 2.(IV)
+
+-- Now we initally think low=mid=0, high=n-1 and the whole array is III section.
+-- If arr[mid]==0, we swap it with arr[low],mid++,low--(0 is added to end of I section)
+-- If arr[mid]==1, we just do mid++(since it is already there in the end of II section)
+-- If arr[mid]==2, we swap it with arr[high],high--(2 is added to start of IV section).
+
+--Thats it,Sorted!
+--O(1)
+
+*/
+class Solution {
+public:
+    void sortColors(vector<int>& nums)
+    {
+        int low=0,mid=0,high=nums.size()-1;
+
+        while(mid<=high){
+        if(nums[mid]==0)
+        {
+            int temp=nums[mid];
+            nums[mid]=nums[low];
+            nums[low]=temp;
+            mid++;
+            low++;
+        }
+        else if(nums[mid]==1)
+        {
+            mid++;
+        }
+        else
+        {
+            int temp=nums[mid];
+            nums[mid]=nums[high];
+            nums[high]=temp;
+            high--;
+        }
+        }
+
+    }
+};
