4cd0cf55ff142d9faa4419d7a7efff683ba3c883
Sw!tchblade <134306795+HemanthK-12@users.noreply.github.com>
2024-06-28 15:57:55 +0530

    bruteforce--O(n^2)

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,28 +2,15 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices)
     {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-        
-        int buy=0;
-        int sell=1;
         int max=0;
-        while(sell<prices.size())
+        for(int i=0;i<prices.size();i++)
         {
-            if( prices[sell]<prices[buy] )
+            for(int j=i+1;j<prices.size();j++)
             {
-                buy=sell;
-                sell++;
-            }
-            else
-            {
-                int profit=prices[sell]-prices[buy];
+                int profit=prices[j]-prices[i];
                 if(profit>max)
                     max=profit;
-                sell++;
             }
-
         }
         return max;
         
