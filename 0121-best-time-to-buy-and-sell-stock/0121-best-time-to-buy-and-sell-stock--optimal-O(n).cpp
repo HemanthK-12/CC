a47137fff1d3da36c36167c01566c951881b9386
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-06-28 16:04:33 +0530

    Time: 55 ms (98.72%), Space: 96 MB (9.13%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock--optimal-O(n).cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock--optimal-O(n).cpp
@@ -1,3 +1,9 @@
+/*
+-- Two - pointer approach
+-- We initialize left pointer to 0 and right pointer to 1 and continue til right pointer reaches the ned.
+-- If left pointer value(buying price) is lesser than right pointer value(selling price), we find profit and equate to max if more than max.Then increment right;
+-- If left pointer value(buying price) is greater than right pointer(selling price), we make left=right(since we got a lower price to buy) and increment right.
+*/
 class Solution {
 public:
     int maxProfit(vector<int>& prices)
