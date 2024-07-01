7b9cc6cb43f4d29ae2616fc4e4843c835dac1225
Sw!tchblade <134306795+HemanthK-12@users.noreply.github.com>
2024-07-01 17:36:19 +0530

    Time: 0 ms (100%), Space: 11.2 MB (7.08%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
     {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         vector<int> final(m+n);
         int i=0,j=0,k=0;
         while(i<m && j<n)
