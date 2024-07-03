3a98e7b8432178ab593fd3748daabd0e53246b28
Sw!tchblade <134306795+HemanthK-12@users.noreply.github.com>
2024-07-03 23:41:12 +0530

    Time: 15 ms (92.47%), Space: 23.9 MB (65.62%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -5,20 +5,26 @@ public:
         ios::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        
-        vector<int> hash(nums.size());
-        vector<int> final;
-
-        for(int i=0;i<nums.size();i++)
+        int n=nums.size();
+        long long sum=0,sos=0,sumActual=0,sosActual=0;
+        for(int i=0;i<n;i++)
         {
-            if(hash[nums[i]-1]==0)
-                hash[nums[i]-1]++;
-            else if(hash[nums[i]-1]==1)
-                final.push_back(nums[i]);
+            sum+=(long long)nums[i];
+            sos+=(long long)((nums[i])*(nums[i]));
         }
-        for(int i=0;i<nums.size();i++)
-            if(hash[i]==0)
-                final.push_back(i+1);
+        sumActual=((n)*(n+1LL))/2LL;
+        sosActual=((n)*(n+1LL)*((2LL*n)+1LL))/6LL;
+
+        long long xyDiff=(sumActual-sum);
+        long long xySum=((sosActual-sos)/xyDiff);
+
+        int y=(int)((xySum+xyDiff)/2);
+        int x=y-(int)xyDiff;
+
+        vector<int> final;
+        final.push_back(x);
+        final.push_back(y);
+
         return final;
         
     }
