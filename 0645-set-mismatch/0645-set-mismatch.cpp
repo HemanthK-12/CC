062f327aa530566953d9bdaccee0e972fa68e0f7
Sw!tchblade <134306795+HemanthK-12@users.noreply.github.com>
2024-07-03 08:27:34 +0530

    Time: 15 ms (92.47%), Space: 24.7 MB (52.94%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums)
+    {
+        vector<int> hash(nums.size());
+        vector<int> final;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(hash[nums[i]-1]==0)
+                hash[nums[i]-1]++;
+            else if(hash[nums[i]-1]==1)
+                final.push_back(nums[i]);
+        }
+        for(int i=0;i<nums.size();i++)
+            if(hash[i]==0)
+                final.push_back(i+1);
+        return final;
+        
+    }
+};
