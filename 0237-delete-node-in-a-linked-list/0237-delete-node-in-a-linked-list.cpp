65bae4a93a04c86246cdda8848d00ad60c39e7c9
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-07-16 02:58:22 +0530

    Update dumbass question brute & optimal

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -5,6 +5,11 @@
  *     ListNode *next;
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
+ * This is the optimal solution.
+ * Instead of trying to delete the node, we copy the contents of the next node to this node.
+ * Then, we connect this node to next of next node
+ * That's it, dumbass question
+ * T=O(1)
  */
 class Solution {
 public:
