87e784419ecaa990138600bb83ff76e554a147e3
Sw!tchblade <134306795+HemanthK-12@users.noreply.github.com>
2024-07-16 01:24:15 +0530

    Time: 0 ms (100%), Space: 14.8 MB (5.07%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -7,44 +7,29 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
- * First traverse through the linked list and get length of linked list.
- * Then take two pointers(1 & 2), one after another, and move lengt-n times.
- * Now, 2 will point to the nthFromEnd node and 1 will point to it's previous node.Connect 1 to 2->next and return head.
- * When n==length, return head->next instead of head.
  */
- 
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* current=head;
-        int c=0;
-        if(current==nullptr || current->next==nullptr)
-            return nullptr;
-        while(current!=nullptr)
+    ListNode* removeNthFromEnd(ListNode* head, int n)
+    {
+        ListNode* a=head;
+        while(n--)
         {
-            c++;
-            current=current->next;
+            a=a->next;
         }
-        if(c==2)
+        if(a==nullptr)
         {
-            if(n==1)
-            {
-                head->next=nullptr;
-                return head;
-            }
-            if(n==2)
-                return head->next;
+            return head->next;
         }
-        int k=c-n+1;
-        current=head;
-        ListNode* prev=head;
-        current=head->next;
-        for(int i=0;i<k-2;i++)
+        ListNode* b=head;
+        while(a->next!=nullptr)
         {
-            prev=prev->next;
-            current=current->next;
+            a=a->next;
+            b=b->next;
         }
-        prev->next=current->next;
+        b->next=b->next->next;
         return head;
+
+        
     }
 };
