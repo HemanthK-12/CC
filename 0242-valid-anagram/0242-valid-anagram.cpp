dbadb93c9408ed5127db5617cee7feb73f4f27dd
Sw!tchblade <134306795+HemanthK-12@users.noreply.github.com>
2024-08-11 06:14:55 +0530

    Time: 11 ms (37.37%), Space: 9.8 MB (21.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,8 +2,26 @@ class Solution {
 public:
     bool isAnagram(string s, string t) 
     {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;        
+        map<char,int> lookup;
+        int c=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s[i];
+            lookup[c]++;
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            char c=t[i];
+            lookup[c]--;
+        }
+        for(auto it:lookup)
+        {
+            if(it.second==0)
+                c++;
+        }        
+        if(c==lookup.size())
+            return true;
+        else
+            return false;
     }
 };
